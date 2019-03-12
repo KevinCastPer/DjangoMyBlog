bae0b5ef3c013693924bda9e554aff02443959d9
KevinCastPer <kecasper@gmail.com>
2019-03-12 12:14:26 +0100

    crear el modelo de Posts

---
+++ miblog/posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=150)
+    contenido = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.titulo
